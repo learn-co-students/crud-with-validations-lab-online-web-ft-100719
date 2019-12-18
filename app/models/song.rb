b902e71c18d99dc2bdc372e040a6bb070809bd1f
Huda Yousif <hjoski@gmail.com>
2019-12-18 01:55:19 -0800

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,7 @@
+class Song < ApplicationRecord
+    include ActiveModel::Validations
+    validates :title, presence:true,  uniqueness: { scope: :release_year }
+    validates :released, inclusion: { in: [true, false] }
+    validates :artist_name, presence: true
+    validates_with SongValidator
+end
